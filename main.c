a1764a24f6a415e1d28c665e76711f96e267ba59
jefferson98br <32877861+jefferson98br@users.noreply.github.com>
2018-04-09 14:52:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,73 @@
 #include "matriz.h"
 
 int main(){
+	/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct tLista lista;
+
+struct tLista
+{
+	char nome[20];
+	int **pMatriz;
+	lista  *prox;
+};
+
+
+int declaraMatriz(lista **inicio, char nomeMatriz[], int dimensaoI, int dimensaoJ)
+{
+	lista *aux;
+	int **M;
+	int i;
+
+	//M = (int *) malloc (dimensaoI * sizeof (int *));
+	//for (i = 0; i < dimensaoI; ++i)
+	//M[i] = (int *) malloc (dimensaoJ * sizeof (int));
+
+
+	aux = (lista *) malloc (sizeof(lista));
+
+	if(!aux) return 1;
+	
+	if(!(*inicio))
+	{
+		strcpy((aux->nome), nomeMatriz);
+		//lista->pMatriz = M;
+		aux->prox = NULL;
+	}
+		
+}
+
+int main()
+{	
+	lista *inicio = NULL;
+	int op, dimensaoI, dimensaoJ;
+	char nomeMatriz[20];
+	do
+	{
+		printf("MENU\n\n");
+		printf("1) Declarar matriz\n");	
+		scanf("%i", &op);
+
+		switch(op)
+		{
+			case 1: printf("Nome da matriz: ");
+				scanf("%s", nomeMatriz);
+
+				printf("Dimensao I: ");
+				scanf("%i", &dimensaoI);
+				printf("Dimensao J: ");
+				scanf("%i", &dimensaoJ);
+				
+				declaraMatriz(&inicio, nomeMatriz, dimensaoI, dimensaoJ);
+		}
+		
+	}while (op != 9);	
+		
+		
+}
+	*/
 
 	return 0;
 }
